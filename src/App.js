fcbee5b1a4f468f3f591120239a766935b4a2765
Student <example@example.com>
2020-02-19 14:15:26 -0800

    initial structure of tamagotchi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Tamagotchi from './Tamagotchi';
+import Footer from './Footer';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      tamagotchiStats: {};
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+      <Header/>
+      <Tamagotchi/>
+      <Footer/>
+      </div>
+    );
+  }
 }
-
+Tamagotchi.propTypes = PropTypes.shape({
+    name: PropTypes.string,
+    foodLevel: PropTypes.number,
+    playLevel: PropTypes.number,
+    cleanLevel: PropTypes.number,
+  });
 export default App;
---
+++ src/components/Tamagotchi.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class Tamagotchi extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      tamagotchiStats: [];
+    }
+  }
+
+render() {
+  return (
+    <div>
+    </div>
+  )
+}
+
+}
